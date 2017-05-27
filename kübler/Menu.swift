eb112c1b0454bcb924e5531a512b9a8a49b33e56
sean <seanlimmac@Seans-MacBook-Pro.local>
2017-05-27 13:50:02 +0800

    build

--- kübler/Menu.swift
+++ kübler/Menu.swift
@@ -17,6 +17,7 @@ class startVC: UIViewController {
 	@IBOutlet weak var playB: UIButton!
 	@IBOutlet weak var logo: UILabel!
 	@IBOutlet weak var bgContainer: UIView!
+    @IBOutlet weak var covveLogo: UIButton!
 	
 	override var prefersStatusBarHidden: Bool{
 		return true
@@ -26,10 +27,10 @@ class startVC: UIViewController {
         super.viewDidLoad()
 		logo.alpha = 0
 		playB.alpha = 0
+        covveLogo.alpha = 0
 		
 		playB.layer.borderWidth = 0.5
 		playB.layer.borderColor = UIColor.white.cgColor
-//		playB.roundify15()
 		
 		bgView = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "menuBG") as! mainMenuBG
 		self.addChildViewController(bgView)
@@ -39,6 +40,7 @@ class startVC: UIViewController {
 		UIView.animate(withDuration: 1, delay: 1, options: .curveEaseInOut, animations: { 
 			self.logo.alpha = 1
 			self.playB.alpha = 1
+            self.covveLogo.alpha = 1
 		}, completion: nil)
 		
 	}
@@ -48,6 +50,7 @@ class startVC: UIViewController {
 		UIView.animate(withDuration: 1, delay: 0, options: .curveEaseInOut, animations: {
 			self.logo.alpha = 0
 			self.playB.alpha = 0
+            self.covveLogo.alpha = 0
 		}, completion: {(s) in
 			self.presentSelectmenu()
 		})
@@ -72,6 +75,7 @@ class startVC: UIViewController {
 		UIView.animate(withDuration: 1, delay: 0, options: .curveEaseInOut, animations: {
 			self.logo.alpha = 1
 			self.playB.alpha = 1
+            self.covveLogo.alpha = 1
 		}, completion: {(s) in
 		})
 	}
@@ -84,7 +88,7 @@ class selectLevelMenu : UIViewController {
 	@IBOutlet var buttonArray: [UIButton]!
 	
 	
-	
+    var prefaceView : preface = preface()
 	var instanceofStart : startVC?
 	
 	override func viewDidLoad() {
@@ -111,18 +115,40 @@ class selectLevelMenu : UIViewController {
 		instanceofStart?.callbackMenu()
 		bgView.exitMenu()
 	}
-	
+
 	@IBAction func menuButtonPressed(_ sender: UIButton) {
 		bgView.movetoRandPosition()
-		switch sender.tag {
-			case 0:
-				break
-			default:
-				break
-		}
+		segueToPrefaceWithIndex(index: sender.tag)
 	}
+    
+    func segueToPrefaceWithIndex(index:Int){
+        prefaceView = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "preface") as! preface
+        prefaceView.chapter = index
+        self.addChildViewController(prefaceView)
+        prefaceView.view.alpha = 0;
+        prefaceView.view.backgroundColor = .clear
+        prefaceView.view.center = self.view.center
+        self.view.addSubview(prefaceView.view)
+        UIView.animate(withDuration: 0.5) {
+            self.backB.alpha = 0;
+            _ = self.buttonArray.map{$0.alpha = 0}
+            self.prefaceView.view.alpha = 1
+        }
+    }
 	
 	
+}
+
+class preface : UIViewController {
+    
+    var chapter: Int? = nil
+    @IBOutlet weak var ContentText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
 }
 
 
