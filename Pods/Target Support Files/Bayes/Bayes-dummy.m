c8fa56ad4c773ef8380a35df200e0e0961755cbf
sean <seanlimmac@Seans-MacBook-Pro.local>
2017-06-18 11:20:03 +0800

    build

--- Pods/Target Support Files/Bayes/Bayes-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Bayes : NSObject
-@end
-@implementation PodsDummy_Bayes
-@end
--- Pods/Target Support Files/Pods-kübler/Pods-kübler-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ku_bler : NSObject
-@end
-@implementation PodsDummy_Pods_ku_bler
-@end
