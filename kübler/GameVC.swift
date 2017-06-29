82972019166bb92545fd6b4a9cf20272d7f364e1
sean <seanlimmac@Seans-MacBook-Pro.local>
2017-06-30 00:03:11 +0800

    build

--- kübler/GameVC.swift
+++ kübler/GameVC.swift
@@ -8,9 +8,42 @@
 
 import UIKit
 import Bayes
+import QuartzCore
+import SceneKit
 
 class GameVC: UIViewController {
 
+    var gameView :SCNView!
+    var gameScene:SCNScene!
+    var cameraNode:SCNNode!
+    var targetCreationTime: TimeInterval = 0
+    
+    func initView(){
+        gameView = self.view as! SCNView
+        gameView.allowsCameraControl = true
+        gameView.autoenablesDefaultLighting = true
+    }
+    
+    func initScene(){
+        gameScene = SCNScene()
+        gameView.scene = gameScene
+        gameView.isPlaying = true
+    }
+    
+    func initCamera() {
+        cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        
+        cameraNode.position = SCNVector3(x:0, y:5,z:10)
+    }
+    
+    func createTarget(){
+        let shape = SCNPyramid.init(width: 1, height: 1, length: 1)
+        shape.materials.first?.diffuse.contents = UIColor.white
+        let shapeNode = SCNNode.init(geometry: shape)
+        
+        gameScene.rootNode.addChildNode(shapeNode)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()        
     }
